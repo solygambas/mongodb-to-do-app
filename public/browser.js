21b81ebafd302ea6fd76489549bd735e6050dfbe
solygambas <solygambas@gmail.com>
2020-04-09 10:39:34 +0200

    add deleting item

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,19 @@
 document.addEventListener("click", function (e) {
+  // Delete Feature
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Do you really want to delete this item permanently?")) {
+      axios
+        .post("/delete-item", {
+          id: e.target.getAttribute("data-id"),
+        })
+        .then(function () {
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch(function () {
+          console.log("Please try again later.");
+        });
+    }
+  }
   // Update Feature
   if (e.target.classList.contains("edit-me")) {
     let userInput = prompt(
